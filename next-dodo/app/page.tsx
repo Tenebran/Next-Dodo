f95d4e3ed10e3e235a719b3bb7a61e1ee89f9008
Sergej Garkusha <Tenebrann@googlemail.com>
2025-01-07 17:13:44 +0100

    Add Button component, update styles and dependencies

--- next-dodo/app/page.tsx
+++ next-dodo/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <h1>
+      Home <Button variant={'outline'}>Корзина</Button>
+    </h1>
+  );
 }
