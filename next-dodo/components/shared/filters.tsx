c1fa07d20bf0a7cf1a743606c26796ad66f598ef
Sergej Garkusha <Tenebrann@googlemail.com>
2025-01-09 20:03:56 +0100

    Add CheckboxFiltersGroup and update Filters component with enhanced filtering features

--- next-dodo/components/shared/filters.tsx
+++ next-dodo/components/shared/filters.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Title } from './title';
 import { FilterCheckbox, RangeSlider } from '.';
 import { Input } from '../ui';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
 
 interface Props {
   className?: string;
@@ -24,6 +25,64 @@ const Filters: React.FC<Props> = ({ className }) => {
         </div>
         <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
       </div>
+
+      <CheckboxFiltersGroup
+        title="Ингридиенты"
+        className="mt-5"
+        limit={6}
+        items={[
+          {
+            text: 'Сырный соус',
+            value: '1',
+          },
+          {
+            text: 'Моцарелла',
+            value: '2',
+          },
+          {
+            text: 'Чеснок',
+            value: '3',
+          },
+          {
+            text: 'Солёные огурчики',
+            value: '4',
+          },
+          {
+            text: 'Красный лук',
+            value: '5',
+          },
+          {
+            text: 'Томаты',
+            value: '6',
+          },
+        ]}
+        defaultItem={[
+          {
+            text: 'Сырный соус',
+            value: '1',
+          },
+          {
+            text: 'Моцарелла',
+            value: '2',
+          },
+          {
+            text: 'Чеснок',
+            value: '3',
+          },
+          {
+            text: 'Солёные огурчики',
+            value: '4',
+          },
+          {
+            text: 'Красный лук',
+            value: '5',
+          },
+          {
+            text: 'Томаты',
+            value: '6',
+          },
+        ]}
+      />
     </div>
   );
 };
