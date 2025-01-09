7f86e7b09eab742e726474e957144f78e6f59fb5
Sergej Garkusha <Tenebrann@googlemail.com>
2025-01-09 18:54:24 +0100

    Add FilterCheckbox and Filter components, update Home component with Filters integration

---
+++ next-dodo/components/shared/top-bar.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { Container } from './container';
+import { Categories } from './categories';
+import { SortPopup } from './sortPopup';
+
+interface Props {
+  className?: string;
+}
+
+const TopBar: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10', className)}>
+      <Container>
+        <Categories clasName="" />
+        <SortPopup className="" />
+      </Container>
+    </div>
+  );
+};
+
+export { TopBar };
