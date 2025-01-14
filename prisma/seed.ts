8b64f8eeb143f2da383ea6b6ed212579b432f312
Sergej Garkusha <Tenebrann@googlemail.com>
2025-01-14 15:57:26 +0100

    feat: add seed script for populating database with test data
    - Added user, category, ingredient, product, productItem, cart, cartItem, story, and storyItem creation logic.
    - Implemented down function to truncate all tables and reset IDs.
    - Introduced generateProductItem utility for dynamic product item creation.
    - Improved structure and data initialization with additional stories and story items.
    - Fixed issues with data relations and cascade deletions.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import { Prisma } from '@prisma/client';
-import { _ingredients, categories, products } from './constants';
+import { categories, _ingredients, products } from './constants';
 import { prisma } from './prisma-client';
 import { hashSync } from 'bcrypt';
 
@@ -28,21 +28,23 @@ async function up() {
   await prisma.user.createMany({
     data: [
       {
-        fullName: 'John Doe',
-        email: '9Y545D0Q@example.com',
-        password: hashSync('123456', 10),
-        verefied: new Date(),
+        fullName: 'User Test',
+        email: 'user@test.ru',
+        password: hashSync('111111', 10),
+        verified: new Date(),
+        role: 'USER',
       },
       {
-        fullName: 'John Doe2',
-        email: '9YD0Q@example.com',
-        password: hashSync('123456', 10),
-        verefied: new Date(),
+        fullName: 'Admin Admin',
+        email: 'admin@test.ru',
+        password: hashSync('111111', 10),
+        verified: new Date(),
+        role: 'ADMIN',
       },
     ],
   });
 
-  await prisma.categoty.createMany({
+  await prisma.category.createMany({
     data: categories,
   });
 
@@ -130,16 +132,110 @@ async function up() {
       generateProductItem({ productId: 17 }),
     ],
   });
+
+  await prisma.cart.createMany({
+    data: [
+      {
+        userId: 1,
+        totalAmount: 0,
+        token: '11111',
+      },
+      {
+        userId: 2,
+        totalAmount: 0,
+        token: '222222',
+      },
+    ],
+  });
+
+  await prisma.cartItem.create({
+    data: {
+      productItemId: 1,
+      cartId: 1,
+      quantity: 2,
+      ingredients: {
+        connect: [{ id: 1 }, { id: 2 }, { id: 3 }],
+      },
+    },
+  });
+
+  await prisma.story.createMany({
+    data: [
+      {
+        previewImageUrl:
+          'https://cdn.inappstory.ru/story/xep/xzh/zmc/cr4gcw0aselwvf628pbmj3j/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=3101815496',
+      },
+      {
+        previewImageUrl:
+          'https://cdn.inappstory.ru/story/km2/9gf/jrn/sb7ls1yj9fe5bwvuwgym73e/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=3074015640',
+      },
+      {
+        previewImageUrl:
+          'https://cdn.inappstory.ru/story/quw/acz/zf5/zu37vankpngyccqvgzbohj1/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=1336215020',
+      },
+      {
+        previewImageUrl:
+          'https://cdn.inappstory.ru/story/7oc/5nf/ipn/oznceu2ywv82tdlnpwriyrq/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=38903958',
+      },
+      {
+        previewImageUrl:
+          'https://cdn.inappstory.ru/story/q0t/flg/0ph/xt67uw7kgqe9bag7spwkkyw/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=2941222737',
+      },
+      {
+        previewImageUrl:
+          'https://cdn.inappstory.ru/story/lza/rsp/2gc/xrar8zdspl4saq4uajmso38/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=4207486284',
+      },
+    ],
+  });
+
+  await prisma.storyItem.createMany({
+    data: [
+      {
+        storyId: 1,
+        sourceUrl:
+          'https://cdn.inappstory.ru/file/dd/yj/sx/oqx9feuljibke3mknab7ilb35t.webp?k=IgAAAAAAAAAE',
+      },
+      {
+        storyId: 1,
+        sourceUrl:
+          'https://cdn.inappstory.ru/file/jv/sb/fh/io7c5zarojdm7eus0trn7czdet.webp?k=IgAAAAAAAAAE',
+      },
+      {
+        storyId: 1,
+        sourceUrl:
+          'https://cdn.inappstory.ru/file/ts/p9/vq/zktyxdxnjqbzufonxd8ffk44cb.webp?k=IgAAAAAAAAAE',
+      },
+      {
+        storyId: 1,
+        sourceUrl:
+          'https://cdn.inappstory.ru/file/ur/uq/le/9ufzwtpdjeekidqq04alfnxvu2.webp?k=IgAAAAAAAAAE',
+      },
+      {
+        storyId: 1,
+        sourceUrl:
+          'https://cdn.inappstory.ru/file/sy/vl/c7/uyqzmdojadcbw7o0a35ojxlcul.webp?k=IgAAAAAAAAAE',
+      },
+    ],
+  });
 }
+
 async function down() {
-  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
 }
 
 async function main() {
   try {
     await down();
     await up();
-  } catch {}
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 main()
