541bec46a8e24949c1dd6540cecb34429c8e07a1
Sergej Garkusha <Tenebrann@googlemail.com>
2025-01-21 15:11:18 +0100

    feat(cart): integrate advanced cart management with DTOs, Zustand, and utility functions

---
+++ shared/lib/get-cart-details.ts
@@ -0,0 +1,37 @@
+import { CartDTO } from '../services/dto/cart.dto';
+
+export type CartStateItem = {
+  id: number;
+  quantity: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  pizzaSize?: number | null;
+  pizzaType?: number | null;
+  ingredients: Array<{ name: string; price: number }>;
+};
+
+interface RetunType {
+  totalAmount: number;
+  items: CartStateItem[];
+}
+
+export const getCartDetails = (data: CartDTO): RetunType => {
+  const items = data.items.map((item) => ({
+    id: item.id,
+    quantity: item.quantity,
+    name: item.productItem.product.name,
+    imageUrl: item.productItem.product.imageUrl,
+    price: item.productItem.product.price,
+    pizzaSize: item.productItem.size,
+    pizzaType: item.productItem.pizzaType,
+    ingredients: item.ingredients.map((ing) => ({
+      name: ing.name,
+      price: Number(ing.price),
+    })),
+  }));
+  return {
+    items,
+    totalAmount: data.totalAmount,
+  };
+};
--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -2,3 +2,4 @@ export { getAvailable } from './get-available';
 export { calcTotalPizzaPrice } from './calc-total-pizza-price';
 export { getPizzaDetails } from './get-pizza-details';
 export { getCartItemsDetails } from './get-cart-items-details';
+export { getCartDetails } from './get-cart-details';
