f6f2bffb3e3b54b4db48692b718601bb6a2905c0
Sergej Garkusha <Tenebrann@googlemail.com>
2025-01-25 20:08:08 +0100

    refactor(checkout): component structure improvements

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,48 +1,47 @@
 'use client';
 
-import {
-  CheckoutItem,
-  CheckoutSeidbar,
-  Container,
-  Title,
-  WhiteBlock,
-} from '@/shared/components/shared';
-import { FormInput } from '@/shared/components/shared/form-components';
+import { CheckoutSeidbar, Container, Title, WhiteBlock } from '@/shared/components/shared';
+import { FormInput } from '@/shared/components/shared/form';
 import { Input, Textarea } from '@/shared/components/ui';
-import { PizzaSizes, PizzaTypes } from '@/shared/constans/pizza';
 import { useCart } from '@/shared/hooks/use-cart';
-import { getCartItemsDetails } from '@/shared/lib';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CheckoutCart } from '@/shared/components/shared/checkout';
 
 export default function CheckoutPage() {
   const { items, totalAmount, onCklickCountButton, removeCartItem } = useCart();
 
+  const checkoutSchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    email: z.string().email(),
+    phone: z.string(),
+    adress: z.string(),
+    comment: z.string(),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(checkoutSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      adress: '',
+      comment: '',
+    },
+  });
   return (
     <Container className="mt-10">
       <Title text="Оформление Заказа" className="font-extrabold mb-8 text-[36px]" />
       <div className="flex gap-10">
         <div className="flex flex-col gap-10 flex-1 mb-20">
-          <WhiteBlock title="1. Корзина">
-            <div className="flex flex-col gap-5">
-              {items.map((item) => (
-                <CheckoutItem
-                  key={item.id}
-                  disabled={item.disabled}
-                  imageUrl={item.imageUrl}
-                  name={item.name}
-                  price={item.price}
-                  details={getCartItemsDetails(
-                    item.pizzaType as PizzaTypes,
-                    item.pizzaSize as PizzaSizes,
-                    item.ingredients
-                  )}
-                  quantity={item.quantity}
-                  id={item.id}
-                  onClickCountButton={(type) => onCklickCountButton(item.id, item.quantity, type)}
-                  onClickRemove={() => removeCartItem(item.id)}
-                />
-              ))}
-            </div>
-          </WhiteBlock>
+          <CheckoutCart
+            items={items}
+            onCklickCountButton={onCklickCountButton}
+            removeCartItem={removeCartItem}
+          />
           <WhiteBlock title="2. Персональные данные">
             <div className="grid grid-cols-2 gap-5">
               <Input name="firstName" className="text-base" placeholder="Имя" />
---
+++ shared/components/shared/checkout/checkout-cart.tsx
@@ -0,0 +1,48 @@
+import { cn } from '@/shared/lib/utils';
+import React from 'react';
+import { WhiteBlock } from '../white-block';
+import { CheckoutItem } from '../checkout-item';
+import { getCartItemsDetails } from '@/shared/lib';
+import { PizzaSizes, PizzaTypes } from '@/shared/constans/pizza';
+import { CartStateItem } from '@/shared/store';
+
+interface Props {
+  className?: string;
+  items: CartStateItem[];
+  onCklickCountButton: (id: number, quantity: number, type: 'plus' | 'minus') => void;
+  removeCartItem: (id: number) => void;
+}
+
+const CheckoutCart: React.FC<Props> = ({
+  className,
+  items,
+  onCklickCountButton,
+  removeCartItem,
+}) => {
+  return (
+    <WhiteBlock title="1. Корзина" className={cn('', className)}>
+      <div className="flex flex-col gap-5">
+        {items.map((item) => (
+          <CheckoutItem
+            key={item.id}
+            disabled={item.disabled}
+            imageUrl={item.imageUrl}
+            name={item.name}
+            price={item.price}
+            details={getCartItemsDetails(
+              item.pizzaType as PizzaTypes,
+              item.pizzaSize as PizzaSizes,
+              item.ingredients
+            )}
+            quantity={item.quantity}
+            id={item.id}
+            onClickCountButton={(type) => onCklickCountButton(item.id, item.quantity, type)}
+            onClickRemove={() => removeCartItem(item.id)}
+          />
+        ))}
+      </div>
+    </WhiteBlock>
+  );
+};
+
+export { CheckoutCart };
