ff092d1dd0e29aeaf4a8763eca4d3683935b61fa
Sergej Garkusha <sergejgarkusha94@gmail.com>
2025-02-17 11:45:06 +0100

    feat: add story modal with react-insta-stories integration

--- shared/components/shared/stories.tsx
+++ shared/components/shared/stories.tsx
@@ -5,6 +5,8 @@ import { Api } from '@/shared/services/api.client';
 import { IStory } from '@/shared/services/stories';
 import React from 'react';
 import { Container } from './container';
+import { X } from 'lucide-react';
+import ReactStories from 'react-insta-stories';
 
 interface Props {
   className?: string;
@@ -51,6 +53,24 @@ const Stories: React.FC<Props> = ({ className }) => {
             src={story.previewImageUrl}
           />
         ))}
+
+        {open && (
+          <div className="absolute left-0 top-0 w-full h-full bg-black/80 flex items-center justify-center z-30">
+            <div className="relative" style={{ width: 520 }}>
+              <button className="absolute -right-10 -top-5 z-30" onClick={() => setOpen(false)}>
+                <X className="absolute top-0 right-0 w-8 h-8 text-white/50" />
+              </button>
+
+              <ReactStories
+                onAllStoriesEnd={() => setOpen(false)}
+                stories={selectedStory?.items.map((item) => ({ url: item.sourceUrl })) || []}
+                defaultInterval={3000}
+                width={520}
+                height={800}
+              />
+            </div>
+          </div>
+        )}
       </Container>
     </>
   );
